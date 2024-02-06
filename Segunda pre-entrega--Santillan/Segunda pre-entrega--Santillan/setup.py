668419db5e2154962269b2fb7c7244350666ac25
Lautaro Ezequiel Santillan <santillanlautaro03@gmail.com>
2024-02-05 21:09:39 -0300

    Add files via upload

---
+++ Segunda pre-entrega--Santillan/Segunda pre-entrega--Santillan/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="2daPre-entrega--Santillan", 
+    version="1.0", 
+    description="Este es mi primer paquete redistribuible",
+    author="Lautaro Santillan", 
+    author_email="santillanlautaro03@gmail.com"
+)
