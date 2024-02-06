b24a3a14f8e0020ef93e1f2d4a4c68de7b91f6f4
Lautaro Ezequiel Santillan <santillanlautaro03@gmail.com>
2024-02-05 21:10:35 -0300

    Delete Segunda pre-entrega--Santillan directory

--- Segunda pre-entrega--Santillan/setup.py
+++
@@ -1,4 +0,0 @@
-from setuptools import setup
-
-setup(name="paqueteSegundaEntrega", version="1.0", description="Este es mi primer paquete redistribuible",
-author="Lautaro Santillan", author_email="santillanlautaro03@gmail.com")
